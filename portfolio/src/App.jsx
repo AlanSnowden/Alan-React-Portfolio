56b93524f04192a2a99877ac59b91722f7a946fc
Alan <alancvsnowden@hotmail.co.uk>
2024-01-30 22:35:15 +0000

    Setup for working in react

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
+import React from 'react'; //Remove this if not working
+
+function App() {
+  return ( <Router>
+
+  </Router>
+  )
+}
+
+export default App
